7b131b9f92ba2f4af594bcfd8ea8468836d3145a
Ash digit <ashdigit44@gmail.com>
2024-09-30 13:36:50 +0100

    refactored css a little bit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from './Component/Home'
-import MyProfile from './Component/MyProfile'
-import Settings from './Component/Settings'
-import Login from './Component/Login'
-import Signup from './Component/SignUp'
-import Statistics from './Component/Statistics'
-import Leaderboard from './Component/Leaderboard'
+import "./App.css";
+import Home from "./Component/Home";
+import MyProfile from "./Component/MyProfile";
+import Settings from "./Component/Settings";
+import Login from "./Component/Login";
+import Signup from "./Component/SignUp";
+import Statistics from "./Component/Statistics";
+import Leaderboard from "./Component/Leaderboard";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/'
-      element={<Home/>}/>
-      <Route path='/login'
-      element={<Login/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/statistics" element={<Statistics />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- front-end/src/Component/Home.jsx
+++ front-end/src/Component/Home.jsx
@@ -1,23 +1,27 @@
-import Keyboard from "./Keyboard"
+import Keyboard from "./Keyboard";
 import { Link } from "react-router-dom";
 
-function Home(){
-
-    return (
-        <>
-        <nav>
+function Home() {
+  return (
+    <>
+      <nav>
         <Link className="link" to="/login">
           Login
         </Link>
-        </nav>
-        <h1>Le Keyboard Warrior</h1>
-        <br/>
-        <br/>
-        <h2>WPM:100 Accuracy: 50%</h2>
-        <Keyboard/>
-        </>
-    )
+        <Link className="link" to="/leaderboard">
+          Leaderboard
+        </Link>
+        <Link className="link" to="/statistics">
+          Statistics
+        </Link>
+      </nav>
+      <h1>Le Keyboard Warrior</h1>
+      <br />
+      <br />
+      <h2>WPM:100 Accuracy: 50%</h2>
+      <Keyboard />
+    </>
+  );
 }
 
-
-export default Home
+export default Home;
