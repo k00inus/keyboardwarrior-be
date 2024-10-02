82fc53f32a5f10ffe0a7bc28b811eb36396fc9b0
kinus00 <usuncodes@gmail.com>
2024-10-02 11:44:15 +0100

    add env variables

--- back-end/config/firebase.js
+++ back-end/config/firebase.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const firebase = require("firebase/app");
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebaseService.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_CONFIG || {});
 
 const {
   getAuth,
