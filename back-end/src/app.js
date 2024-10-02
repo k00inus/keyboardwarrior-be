e671e5d54496ca5ccdb7dee6a21bdcdab0f0f8ad
kinus00 <usuncodes@gmail.com>
2024-10-02 10:21:51 +0100

    setup backend repo and authentication

---
+++ back-end/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const router = require("./routes");
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(router);
+
+app.get("/", (req, res) => {
+  res.send("Keboard Warrior");
+});
+
+module.exports = app;
