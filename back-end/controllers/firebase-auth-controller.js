6e518815aa35351546a4fc1d498e9d3c18f5c65e
kinus00 <usuncodes@gmail.com>
2024-10-02 15:04:39 +0100

    update registerUser

--- back-end/controllers/firebase-auth-controller.js
+++ back-end/controllers/firebase-auth-controller.js
@@ -23,7 +23,7 @@ class FirebaseAuthController {
         sendEmailVerification(auth.currentUser)
           .then(() => {
             res.status(201).json({
-              user: auth.currentUser,
+              uid: auth.currentUser.uid,
               message: "Verification email sent! User created successfully!",
             });
           })
