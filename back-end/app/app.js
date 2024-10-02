5dcc84e1f2dd5e0c871415b04db0a0da7aaeb680
kinus00 <usuncodes@gmail.com>
2024-10-02 14:13:38 +0100

    allow cross origin resource sharing

--- back-end/app/app.js
+++ back-end/app/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const router = require("../routes");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
